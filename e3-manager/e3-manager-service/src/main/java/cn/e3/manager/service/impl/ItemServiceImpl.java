54724112f4ae858440fb6ca01eed37f76c089ed0
tian7599 <18612137599@163.com>
2017-12-05 22:28:25 +0800

    e3-manager

---
+++ e3-manager/e3-manager-service/src/main/java/cn/e3/manager/service/impl/ItemServiceImpl.java
@@ -0,0 +1,20 @@
+package cn.e3.manager.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cn.e3.manager.service.ItemService;
+import cn.e3.mapper.TbItemMapper;
+import cn.e3.pojo.TbItem;
+@Service
+public class ItemServiceImpl implements ItemService{
+
+	@Autowired
+	private TbItemMapper tbItemMapper;
+	
+	public TbItem findByTbItemId(Long id) {
+		return tbItemMapper.selectByPrimaryKey(id);
+	}
+	
+
+}
